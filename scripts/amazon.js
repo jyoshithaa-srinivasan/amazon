501177e896d48b1cb5401a09e6837285ff3e958e
jyoshithaa_srinivasan <jyoshithaasrinivasan@gmail.com>
2024-08-11 11:02:31 +0530

    updated cartQuantity html to change dynamically

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -91,9 +91,17 @@ document.querySelectorAll('.js-add-to-cart')
 
            }
 
+           let cartQuantity=0;
+
+           cart.forEach((item)=>{
+
+                cartQuantity+=item.quantity;
+           });
+
+           document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+           console.log(cartQuantity);
            console.log(cart);
-
-
            
         });
 
