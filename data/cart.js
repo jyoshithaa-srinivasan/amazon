27d5d86468edc08a02deab386240c6d890c99d2d
jyoshithaa_srinivasan <jyoshithaasrinivasan@gmail.com>
2024-08-12 00:02:39 +0530

    generated html for checkout page order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,17 @@
-export const cart=[];
+// we will not store the products image/ price . we  ll use it from the products.js using product id
+//no need to save the data twice
+//this technique is re-duplicating the data/ normalizing the data
+export const cart=[ {
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2,
+
+} ,
+{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1,
+
+}
+];
 
 export function addToCart(productId){
     let AddQuantity=Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
