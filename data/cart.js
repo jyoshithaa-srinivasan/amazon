7a8616472fb9cfbba470269b14fdba2681c784ef
jyoshithaa_srinivasan <jyoshithaasrinivasan@gmail.com>
2024-12-16 22:37:59 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,14 @@
 // we will not store the products image/ price . we  ll use it from the products.js using product id
 //no need to save the data twice
 //this technique is re-duplicating the data/ normalizing the data
-export let cart=[ {
+
+//  instead of default value , get the values from localstorage
+
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+
+    cart=[ {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2,
 
@@ -10,8 +17,14 @@ export let cart=[ {
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity:1,
 
+}];
+
 }
-];
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+    //localstorage can save only strings
+}
 
 export function addToCart(productId){
     let AddQuantity=Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
@@ -39,8 +52,11 @@ export function addToCart(productId){
                 // productId,
                 // quantity
                });
+            
 
            }
+
+           saveToStorage();
 
 }
 
@@ -53,6 +69,7 @@ export function removeFromCart(productId){
     });
 
     cart=newCart;
+    saveToStorage();
 
 }
 
