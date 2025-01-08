496ca978b6f1af66f3761bb7508a97063defa055
jyoshithaa_srinivasan <jyoshithaasrinivasan@gmail.com>
2025-01-08 22:19:39 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,12 +7,8 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
-hello();
-
-const today=dayjs();
-const deliveryDate=today.add(7,'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary(){
   let cartSummaryHTML='';
@@ -141,13 +137,15 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     const {productId}=link.dataset;
     
     //delete from cart
-    removeFromCart(productId);
+    removeFromCart(productId);  //updates the data when we click delete
     console.log(cart);
 
     const container=document.querySelector(`.js-cart-item-container-${productId}`);
 
     //delete the html
     container.remove();
+
+    renderPaymentSummary(); //regenerate html
 
     updateCartQuantity();
 
@@ -164,6 +162,7 @@ document.querySelectorAll('.js-delivery-option')
       const {productId,deliveryOptionId}=element.dataset;
       updateDeliveryOption(productId,deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
 
     })
   });
